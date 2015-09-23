280b5db7e98677d1c375b50a75b77cc44b4333d6
Oliver Schulz <oschulz@mpp.mpg.de>
2015-09-23 23:08:00 +0200

    BCHistogramFitter::LogLikelihood improvements

--- models/base/BCHistogramFitter.cxx
+++ models/base/BCHistogramFitter.cxx
@@ -163,7 +163,7 @@ double BCHistogramFitter::LogLikelihood(const std::vector<double>& params)
     // the vector elements are not stored consecutively in memory.
     // however it is much faster than copying the contents, especially
     // for large number of parameters
-    fFitFunction[c]->SetParameters(&params[0]);
+    if (fFlagIntegration) fFitFunction[c]->SetParameters(&params[0]);
 
     // get the number of bins
     int nbins = fHistogram->GetNbinsX();
@@ -178,11 +178,12 @@ double BCHistogramFitter::LogLikelihood(const std::vector<double>& params)
         double y = fHistogram->GetBinContent(ibin);
 
         double yexp = 0.;
+        const double *paramsPtr = &params[0];
 
         if (fFlagIntegration)				// use ROOT's TH1D::Integral method
             yexp = fFitFunction[c]->Integral(xmin, xmax);
         else												// use linear interpolation
-            yexp = (fFitFunction[c]->Eval(xmin) + fFitFunction[c]->Eval(xmax)) * (xmax - xmin) / 2.;
+            yexp = (fFitFunction[c]->EvalPar(&xmin, paramsPtr) + fFitFunction[c]->EvalPar(&xmin, paramsPtr)) * (xmax - xmin) / 2.;
 
         // get the value of the Poisson distribution
         loglikelihood += BCMath::LogPoisson(y, yexp);
